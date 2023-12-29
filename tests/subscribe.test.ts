02d0f624226670619e2c7bb98169ccf20af0cf56
Emir Kugic <emir.kugic@hotmail.com>
2023-12-29 19:37:25 +0100

    subscribe

--- tests/subscribe.test.ts
+++ tests/subscribe.test.ts
@@ -3,33 +3,31 @@ import { HomePage } from "../core/page-objects/home-page";
 import { createDriver, quitDriver } from "../core/config/driver-setup";
 import { readFileSync } from "fs";
 import * as path from "path";
-import { LoginPage } from "../core/page-objects/login-page";
+import { SubscribePage } from "../core/page-objects/subscribe-page";
 
 const dataFilePath = path.resolve(__dirname, "../core/data/data.json");
 const testData = JSON.parse(readFileSync(dataFilePath, "utf8"));
 
 let driver: WebDriver;
 let homePage: HomePage;
-let loginPage: LoginPage;
+let subscribePage: SubscribePage;
 
 beforeAll(async () => {
 	driver = await createDriver(testData.url.homepage);
 	homePage = new HomePage(driver);
-	loginPage = new LoginPage(driver);
-}, 50000);
+	subscribePage = new SubscribePage(driver);
+}, 100000);
 
-test("Should login the user", async () => {
+test("Should subscribe", async () => {
 	await homePage.closePopUp();
 	await homePage.selectEurope();
 	await homePage.selectArea();
-	await homePage.clickSignIn();
-	await loginPage.waitForMenu();
-	await loginPage.enterNumber();
-	await loginPage.enterPassword();
-	await loginPage.clickSignIn();
-	await loginPage.checkInformation();
-}, 70000);
+	await homePage.closeCookies();
+	await subscribePage.scrollToBottom();
+	await subscribePage.loadSubscribeBanner();
+	await subscribePage.clickSubscribeBanner();
+}, 100000);
 
 afterAll(async () => {
 	await quitDriver(driver);
-}, 50000);
+}, 100000);
